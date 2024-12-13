0b3b5d8e2458db7e6b9ccde9c5ea7486be6a2be4
Apiracha57 <mohdammarp12@gmail.com>
2024-12-12 19:16:44 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -6,7 +6,7 @@ URL: http://localhost:8501/#nuclear-explosions-data-explorer
 
 Description: This dataset provides a comprehensive record of nuclear explosions conducted worldwide between 1945 and 1998.
 """
-pip install matplotlib
+
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
